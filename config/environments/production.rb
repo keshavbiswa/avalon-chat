f9d265d128c7c0fa553858ff2f9b0c9f73b75edf
Keshav <keshavbiswa21@gmail.com>
2018-06-12 17:36:06 +0530

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ["https://avalonchat.herokuapp.com", "http://avalonchat.herokuapp.com"]
+  config.action_cable.url = "wss://avalonchat.herokuapp.com/cable"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
       post :close
     end
   end
+  mount ActionCable.server => '/cable'
 end
